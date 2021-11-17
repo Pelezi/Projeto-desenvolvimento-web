fe48cc365726d47dcb450686fcd7ee891a7fd47a
Pelezi <abcsandro@hotmail.com>
2021-11-17 09:30:29 -0300

    Fix dropdown

---
+++ produtos.js
@@ -0,0 +1,11 @@
+function FbotonOn() { 
+
+    if(document.getElementById('frutas').checked)
+        null
+    else if(document.getElementById('vegetais').checked)
+    null
+    else if(document.getElementById('legumes').checked)
+    null
+    else
+    alert('checked')
+}
