42672f45fcb3f26671f8ca836638fbc104db17f7
HigorMateus <higormateus364@gmail.com>
2021-11-18 13:44:05 -0300

    main

--- pagina_compra.js
+++ pagina_compra.js
@@ -21,6 +21,7 @@ $(document).ready(function(){
 });
 
 const switcher = document.querySelector("#themeswitcher");
+const cartswitcher = document.querySelector("#cart");
 
 function themeswitch() {
 
@@ -30,18 +31,24 @@ function themeswitch() {
         document.getElementById("navbartop").className += " navbar-light";
         document.getElementById("navbartop").className += " bg-light";
         document.getElementById("footerbottom").className += " bg-light";
+        document.getElementById("carrinho").className += " btn-light";
         document.getElementById("navbartop").className = document.getElementById("navbartop").className.replace( /(?:^|\s)navbar-dark(?!\S)/g , '' )
         document.getElementById("navbartop").className = document.getElementById("navbartop").className.replace( /(?:^|\s)bg-dark(?!\S)/g , '' )
         document.getElementById("footerbottom").className = document.getElementById("footerbottom").className.replace( /(?:^|\s)bg-dark(?!\S)/g , '' )
+        document.getElementById("carrinho").className = document.getElementById("carrinho").className.replace( /(?:^|\s)btn-dark(?!\S)/g , '' )
+        cartswitcher.setAttribute('src', 'Images/shopping-cart.png');
     }else{
         document.documentElement.setAttribute('data-theme', 'dark')
         switcher.setAttribute('src', 'Images/sun-64x64.png')
         document.getElementById("footerbottom").className += " bg-dark";
         document.getElementById("navbartop").className += " navbar-dark";
         document.getElementById("navbartop").className += " bg-dark";
+        document.getElementById("carrinho").className += " btn-dark";
         document.getElementById("footerbottom").className = document.getElementById("footerbottom").className.replace( /(?:^|\s)bg-light(?!\S)/g , '' )
         document.getElementById("navbartop").className = document.getElementById("navbartop").className.replace( /(?:^|\s)navbar-light(?!\S)/g , '' )
         document.getElementById("navbartop").className = document.getElementById("navbartop").className.replace( /(?:^|\s)bg-light(?!\S)/g , '' )
+        document.getElementById("carrinho").className = document.getElementById("carrinho").className.replace( /(?:^|\s)btn-light(?!\S)/g , '' )
+        cartswitcher.setAttribute('src', 'Images/shopping-cart-white.png');
 
     }
 }
