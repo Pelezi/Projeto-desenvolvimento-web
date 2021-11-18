2f5c676ab57585cd6af06369e75f12d679132b97
HigorMateus <higormateus364@gmail.com>
2021-11-17 23:11:08 -0300

    main

--- pagina_compra.js
+++ pagina_compra.js
@@ -20,11 +20,47 @@ $(document).ready(function(){
     });
 });
 
+const switcher = document.querySelector("#themeswitcher");
+
+function themeswitch() {
+
+    if(document.getElementById('checkboxdarkmode').checked){
+        document.documentElement.setAttribute('data-theme', 'light')
+        switcher.setAttribute('src', '/Images/NicePng_moon-png_60348.png')
+        document.getElementById("navbartop").className += " navbar-light";
+        document.getElementById("navbartop").className += " bg-light";
+        document.getElementById("footerbottom").className += " bg-light";
+        document.getElementById("navbartop").className = document.getElementById("navbartop").className.replace( /(?:^|\s)navbar-dark(?!\S)/g , '' )
+        document.getElementById("navbartop").className = document.getElementById("navbartop").className.replace( /(?:^|\s)bg-dark(?!\S)/g , '' )
+        document.getElementById("footerbottom").className = document.getElementById("footerbottom").className.replace( /(?:^|\s)bg-dark(?!\S)/g , '' )
+    }else{
+        document.documentElement.setAttribute('data-theme', 'dark')
+        switcher.setAttribute('src', '/Images/sun-64x64.png')
+        document.getElementById("footerbottom").className += " bg-dark";
+        document.getElementById("navbartop").className += " navbar-dark";
+        document.getElementById("navbartop").className += " bg-dark";
+        document.getElementById("footerbottom").className = document.getElementById("footerbottom").className.replace( /(?:^|\s)bg-light(?!\S)/g , '' )
+        document.getElementById("navbartop").className = document.getElementById("navbartop").className.replace( /(?:^|\s)navbar-light(?!\S)/g , '' )
+        document.getElementById("navbartop").className = document.getElementById("navbartop").className.replace( /(?:^|\s)bg-light(?!\S)/g , '' )
+
+    }
+}
+
+function mascara(t, mask){
+    var i = t.value.length;
+    var saida = mask.substring(1,0);
+    var texto = mask.substring(i)
+    if (texto.substring(0,1) != saida){
+    t.value += texto.substring(0,1);
+    }
+}
+
 let imagens = document.querySelectorAll('.small_img');
 let modal = document.querySelector('.modal-content');
 let modalImg = document.querySelector('#modal_img');
 let btClose = document.querySelector('#bt_close');
 let srcVal="";
+var CEP = document.querySelector('#cep')
 
 for(let i =0; i<imagens.length;i++){
     imagens[i].addEventListener('click',function(){
@@ -39,4 +75,3 @@ for(let i =0; i<imagens.length;i++){
 btClose.addEventListener('click', function(){
     modal.classList.toggle('modal_active');
 });
-
