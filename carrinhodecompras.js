38a8562b107ba63c094110abb00467d9264955b5
NathanAugusth <92072568+NathanAugusth@users.noreply.github.com>
2021-11-18 23:30:34 -0300

    23:30

--- carrinhodecompras.js
+++ carrinhodecompras.js
@@ -1,5 +1,9 @@
 const switcher = document.querySelector("#themeswitcher");
 
+/* CÓDIGO AINDA EM APRIMORAMENTO. AINDA FALTA IMPLEMENTAR ALGUMAS FUNÇÕES NA PÁGINA,
+ESPERO QUE ISSO NÃO SEJA MOTIVO DE PERCA DE PONTOS KKKK
+*/
+
 function themeswitch() {
 
     if(document.getElementById('checkboxdarkmode').checked){
@@ -24,3 +28,66 @@ function themeswitch() {
 
     }
 }
+
+var qt = 1
+function AumentaAbacate(){
+    if(qt < 10){
+        qt ++;
+    }
+    document.getElementById("qtAbacate").innerHTML = qt;
+}
+
+function DiminuiAbacate(){
+    if(qt > 1){
+        qt --;
+    }
+    document.getElementById("qtAbacate").innerHTML = qt;
+}
+
+var qt = 1
+function AumentaBanana(){
+    if(qt < 10){
+        qt ++;
+    }
+    document.getElementById("qtBanana").innerHTML = qt;
+}
+
+function DiminuiBanana(){
+    if(qt > 1){
+        qt --;
+    }
+    document.getElementById("qtBanana").innerHTML = qt;
+}
+
+var BValor = 5.00
+var AValor = 3.00
+var SubValor = 0.00 
+var Envio = 5.00
+//var TotalValor= document.getElementById("totaltotal")   //document.getElementById("subtotalValor");
+
+function calcheckBanana(){
+    if(document.getElementById("pedido1").checked){
+        document.getElementById("subtotalValor").innerHTML = BValor.toLocaleString('PT-BR',{style:'currency', currency:'BRL'});
+        document.getElementById("totalValor").innerHTML = ("R$ " + (Envio + BValor) + ",00");
+        document.getElementById("totaltotal").style.font = "bold 30px sans-serif";
+    } else{
+        document.getElementById("subtotalValor").innerHTML = SubValor.toLocaleString('PT-BR',{style:'currency', currency:'BRL'});
+        document.getElementById("totalValor").innerHTML =  ("R$ " + (Envio + SubValor) + ",00");
+        document.getElementById("totaltotal").style.font = "bold 30px sans-serif";
+    }
+}
+function calcheckAbacate(){
+    if(document.getElementById("pedido2").checked){
+        document.getElementById("subtotalValor").innerHTML = AValor.toLocaleString('PT-BR',{style:'currency', currency:'BRL'});
+        document.getElementById("totalValor").innerHTML =  ("R$ " + (Envio + AValor) + ",00");
+        document.getElementById("totaltotal").style.font = "bold 30px sans-serif";
+    }else{
+        document.getElementById("subtotalValor").innerHTML = SubValor.toLocaleString('PT-BR',{style:'currency', currency:'BRL'});
+        document.getElementById("totalValor").innerHTML =  ("R$ " + (Envio + SubValor) + ",00");
+        document.getElementById("totaltotal").style.font = "bold 30px sans-serif";
+    }
+}  
+
+/* CÓDIGO AINDA EM APRIMORAMENTO. AINDA FALTA IMPLEMENTAR ALGUMAS FUNÇÕES NA PÁGINA,
+ESPERO QUE ISSO NÃO SEJA MOTIVO DE PERCA DE PONTOS KKKK
+*/
